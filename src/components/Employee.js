aa7a1091abfeebec03265fefcdf3e203176eb892
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-02-27 15:46:00 -0800

    about to go over tailwindcss

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,6 @@ function Employee(props) {
 	return (
 		<>
 			<h3>Employee {props.name}</h3>
-			<p>{props.role ? props.role : 'No role'}</p>
 			{props.role ? <p className="role">{props.role}</p> : <p className="no-role">No role</p>}
 		</>
 	)
